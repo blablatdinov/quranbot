1742541bdd252bab5b81178040193f54ac22e8c6
Almaz <a.ilaletdinov@yandex.ru>
2020-02-08 12:42:57 +0300

    change celery task, now it work correct. Change bot.models

--- bot/models.py
+++ bot/models.py
@@ -2,9 +2,14 @@ from django.db import models
 
 
 class QuranAyat(models.Model):
-    content = models.TextField()
-    sura = models.IntegerField()
-    ayat = models.CharField(max_length=5)
+    content = models.TextField(blank=True)
+    arab_text = models.TextField(blank=True)
+    trans = models.TextField(blank=True)
+    audio_link = ...
+    tg_audio_link = ...
+    sura = models.IntegerField(blank=True)
+    ayat = models.CharField(max_length=5, blank=True)
+    html = models.TextField(blank=True)
 
     def __str__(self):
         return f'{self.sura}:{self.ayat}'
@@ -16,9 +21,11 @@ class QuranAyat(models.Model):
 
 class QuranOneDayContent(models.Model):
     content = models.TextField()
+    sura_ayat = models.ForeignKey(QuranAyat, blank=True, on_delete=models.CASCADE)
+    day = models.ForeignKey()
 
     def __str__(self):
-        return f'{self.pk} день'
+        return f'{self.day} день'
 
     class Meta:
         verbose_name = 'Ежедневный контент для пользователей:'
@@ -27,7 +34,8 @@ class QuranOneDayContent(models.Model):
 
 class Subscribers(models.Model):
     telegram_chat_id = models.IntegerField()
-    day = models.ManyToManyField('QuranOneDayContent', blank=True, related_name='subscribers')
+    day = models.IntegerField()
+    status = models.BooleanField(default=True)
 
     def __str__(self):
         return str(self.telegram_chat_id)
--- bot/tasks.py
+++ bot/tasks.py
@@ -2,17 +2,27 @@ from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 
 from celery.task import periodic_task
+from .models import Subscribers, QuranOneDayContent
 from celery.schedules import crontab
 from datetime import timedelta
 from .views import tbot
 
+
 # celery worker -A quranbot --loglevel=info
 # celery -A quranbot beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler
-@periodic_task(run_every=timedelta(seconds=5), name='mft')
+@periodic_task(run_every=timedelta(seconds=5), name='mailing')
 # @shared_task
-def mft():
-    tbot.send_message(chat_id=358610865, text='celery')
-    print('celery')
+def mailing():
+    subs = Subscribers.objects.filter(status=True)  # Получаем подписчиков
+    for sub in subs:  # Проходимся по подписчикам
+        content = QuranOneDayContent.objects.filter(day=sub.day)  # Получаем контент для подписчика
+        try:
+            tbot.send_message(chat_id=sub.telegram_chat_id, text=content)
+            sub.day += 1
+            sub.save()
+        except:
+            sub.status = False
+            sub.save()
 
 
 @shared_task
