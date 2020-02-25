722c691b827f2741ba6a3262886fd4989d1339d4
Almaz <a.ilaletdinov@yandex.ru>
2020-02-25 12:34:36 +0300

    Add message track
    Former-commit-id: 59ea6abf2f00b83d242624859b1d3ca0eee513f0
    Former-commit-id: 73a5e239f315f53dd3a03ea6a689201448743d21
    Former-commit-id: 9a541bec2aa24bb5c8f16a9201703768d9d79257

--- bot/admin.py
+++ bot/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from bot.models import QuranAyat, QuranOneDayContent, Subscribers, Audio
+from bot.models import *
 
 
 admin.site.register(QuranAyat)
 admin.site.register(QuranOneDayContent)
 admin.site.register(Subscribers)
 admin.site.register(Audio)
+admin.site.register(Message)
 
