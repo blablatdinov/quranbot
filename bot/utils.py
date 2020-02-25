848dbcd05abef631735bad63eea92793e77aadfb
Almaz <a.ilaletdinov@yandex.ru>
2020-02-25 11:54:01 +0300

    from prod server 25.02.2020
    Former-commit-id: 6a337b9441f2e8422040b4c4668d5d4cc8be15a9

--- bot/utils.py
+++ bot/utils.py
@@ -1,28 +1,3 @@
-def write_audio(audio_title, audio_link):
-    import csv
-    from bot.models import Audio
-
-    n = 0
-    with open('audio.csv', 'r', encoding='utf-8') as f:
-        data = csv.reader(f)
-        for row in data:
-            if n % 2 == 0:
-                a = Audio.objects.create(title=row[0], audio_link=row[1])
-                a.save()
-                print(n/2)
-            n += 1
-
-
-def write_ayats():
-    from .models import QuranAyat
-    with open('output.txt', 'r', encoding='utf-8') as f:
-        while True:
-            line = eval(f.readline())
-            q = QuranAyat(html=line[1])
-            q.save()
-            print(line[0])
-
-
 def get_arab_text(soup):
     return soup.find('div', class_='quran-speaker').text
 
@@ -43,19 +18,16 @@ def get_audio(soup):
 def pars_ayatss():
     from .models import QuranAyat
     from bs4 import BeautifulSoup
-    
+
     qs = QuranAyat.objects.all()
     for q in qs:
         html = q.html
         soup = BeautifulSoup(html, 'lxml')
         #q.content = soup.find('div', class_='text').find('p').text
         h3 = soup.find('h3').text.split(':')
-        #print(get_audio(soup))
-        #print(h3)
         #q.arab_text = get_arab_text(soup)
         #q.trans = get_trans(soup)
         q.audio_link = get_audio(soup)
-        #print(q.arab_text)
         #q.sura = h3[0]
         #q.ayat = h3[1]
         q.save()
@@ -80,8 +52,6 @@ def save_mp3():
             tbot.delete_message(358610865, msg.message_id)
         else:
             pass
-        #except:
-        #print(f'problem with audion\npk: {audio.pk}\ntitle: {audio.title}')
 
 
 def save_ayat_audio():
