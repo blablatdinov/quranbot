664f0a917e97841dae36c4db1c67df28830101a8
almaz <a.ilaletdinov@yandex.ru>
2020-03-03 21:18:31 +0300

    from manjaro
    Former-commit-id: 7f0c811e1e1bf0c3be40e47d4b75e28675b8e6a2

--- content_gen/views.py
+++ content_gen/views.py
@@ -6,11 +6,9 @@ from bot.models import *
 def content_gen(request):
     if request.method == 'GET':
         qs = QuranOneDayContent.objects.all().order_by('-pk')
-        print(qs)
         context = {
             'qs': qs
         }
-        print(context)
         return render(request, 'content_gen.html', context)
     else:
         sura = request.POST['sura']
@@ -25,3 +23,18 @@ def content_gen(request):
             'data': data
         }
         return JsonResponse(response)
+
+
+def send_content(request):
+    data = request.POST
+    for key in data:
+        print(data[key])
+    taken_content_day = data['content_day']
+    taken_sura = data['ayats[]'].split(':')[0]
+    taken_ayat = data['ayats[]']
+    # print(taken_ayat)
+    # print('asdf', taken_sura, taken_ayat)
+    content_day = QuranOneDayContent.objects.get_or_create(day=taken_content_day)
+    content_day = QuranOneDayContent.objects.get_or_create(day=taken_content_day)
+    # print(content_day, ayats)
+    return JsonResponse({'ok': 'true'})
