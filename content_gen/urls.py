56be556cbfc937342ea3782b0649f5cf5689c4a4
almaz <a.ilaletdinov@yandex.ru>
2020-03-03 21:18:31 +0300

    from manjaro
    Former-commit-id: 6f6d6bf288d4fb9f6cf31a6ecfaae27386accf4d
    Former-commit-id: bda0a25e8d9c1c6756181cc7a1ec8a2b289356c8
    Former-commit-id: 8d1653d32c427f42dc45f5086b1b63d9b54ab9f5

--- content_gen/urls.py
+++ content_gen/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from content_gen.views import content_gen
+from content_gen.views import content_gen, send_content
 
 
 urlpatterns = [
-    path('', content_gen)
+    path('', content_gen),
+    path('/send', send_content)
 ]
