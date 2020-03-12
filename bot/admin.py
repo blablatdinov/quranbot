88b3947cf956e4c48662db7fdf29c18c196c4da1
Almaz <a.ilaletdinov@yandex.ru>
2020-03-12 23:10:49 +0300

    from server 12.03.2020

--- bot/admin.py
+++ bot/admin.py
@@ -2,9 +2,16 @@ from django.contrib import admin
 from bot.models import *
 
 
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'date', 'from_user_id', 'message_id', 'chat_id', 'text', )
+
+
+class SubsAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'status')
+
 admin.site.register(QuranAyat)
 admin.site.register(QuranOneDayContent)
-admin.site.register(Subscribers)
+admin.site.register(Subscribers, SubsAdmin)
 admin.site.register(Audio)
-admin.site.register(Message)
+admin.site.register(Message, MessageAdmin)
 
