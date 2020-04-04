bb6d886f9bb9e0fcbbaf576d72dbb335b6b0f3c4
Almaz <a.ilaletdinov@yandex.ru>
2020-04-04 23:42:14 +0300

    from server 04.04.2020

--- bot/tasks.py
+++ bot/tasks.py
@@ -3,7 +3,7 @@ from celery import shared_task
 from telebot.apihelper import ApiException
 
 from celery.task import periodic_task
-from .models import Subscribers, QuranOneDayContent, QuranAyat
+from .models import Subscribers, QuranOneDayContent, QuranAyat, Audio
 from celery.schedules import crontab
 from datetime import timedelta
 from .views import tbot
@@ -49,4 +49,27 @@ def pr_time():
     f"Ястү: {columns[n_for_columns + 6].text}"
     msg = tbot.send_message(sub, res)
     save_message(msg)
+
+
+#@periodic_task()
+def parse_audio():
+    urls = ['https://umma.ru/audlo/shamil-alyautdinov/']
+    last_audio_in_db = Audio.objects.last().title
+    count = 0
+    main_while_break_flag = False
+
+    while True:
+        soup = bs(requests.get(urls[count]).text, 'lxml')
+        articles = soup.find_all('article')
+        print(len(articles))
+        for article in articles:
+            if article.find('h2').text == last_audio_in_db:
+                main_while_break_flag = True
+                break
+            print(article.text)
+            article_link = 'https://umma.ru' + article.find('a')['href']
+            article_soup = bs(requests.get(article_link).text, 'lxml')
+            print(article_soup)
+            audio_download_link = article_soup.find('a', class_='download-audio')['href']
+        exit()
 
