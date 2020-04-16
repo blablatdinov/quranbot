34bb4d0ca2df157c229666da22b9996c1b7d81a2
Almaz <a.ilaletdinov@yandex.ru>
2020-04-16 20:30:58 +0300

    from server 16.04.2020

--- quranbot/celery.py
+++ quranbot/celery.py
@@ -15,6 +15,7 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
+app.conf.timezone = 'Europe/Moscow'
 
 
 @app.task(bind=True)
