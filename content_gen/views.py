d3a45deb5b7095424199a223d2a44df59ada38a1
Almaz <a.ilaletdinov@yandex.ru>
2020-06-20 19:06:25 -0400

    migrate content gen service to vuejs, update settings, now all bot info contain in NamedTuple
    Former-commit-id: 9567f9ae51a14d78cfbd584b2c3aba869769e9f4
    Former-commit-id: 0f84f978e4bf50ed1fb07c41da83e4c878800b7f
    Former-commit-id: 306e749686db775ce5bc3b50f8f763e983524d1a

--- content_gen/views.py
+++ content_gen/views.py
@@ -2,20 +2,26 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from bot.models import *
 
+import json
+
 
 def content_gen(request):
     if request.method == 'GET':
         # print(request.user)
-        qs = QuranOneDayContent.objects.all().order_by('-day')
+        qs = QuranOneDayContent.objects.all().order_by('-day')#[:10]
+        for q in qs:
+            print(q)
         context = {
             'qs': qs
         }
-        return render(request, 'content_gen.html', context)
+        return render(request, 'content_gen_vue.html', context)
     else:
-        # print(request.POST)
+        print(request.body)
         # print(request.method)
         print('take')
-        sura = request.POST['sura']
+        data = eval(request.body)
+        sura = data.get('sura')
+        #qs = QuranAyat.objects.filter(sura=sura, one_day_content=None).order_by('pk')
         qs = QuranAyat.objects.filter(sura=sura).order_by('pk')
         data = []
         for q in qs:
@@ -30,13 +36,14 @@ def content_gen(request):
 
 
 def send_content(request):
-    data = request.POST
-    taken_ayats = data.getlist('ayat[]')
+    data = json.loads(request.body)
+    print(data)
+    taken_ayats = data.get('ayat')
     taken_day = data.get('content_day')
     content_day = QuranOneDayContent.objects.get_or_create(day=taken_day)[0]
     for i in range(len(taken_ayats)):
-        taken_sura = taken_ayats[i].split(':')[0]
-        taken_ayat = taken_ayats[i].split(':')[1]
+        taken_sura = taken_ayats[i].get('sura')
+        taken_ayat = taken_ayats[i].get('ayat')
         ayat = QuranAyat.objects.filter(sura=taken_sura, ayat=taken_ayat)[0]
         # print(ayat)
         ayat.one_day_content = content_day
