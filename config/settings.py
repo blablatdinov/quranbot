b3179746b7c8d0db163769df0ff0e74d1216a480
Almaz <a.ilaletdinov@yandex.ru>
2020-08-08 16:46:25 -0400

    config connect to postgres
    Former-commit-id: 7214e0226603e54addb8617fd2607c1af8e28f4a
    Former-commit-id: 67d1c446cf3c3fd987ef7a5641d5677c96227967
    Former-commit-id: 381fb176bdb1eaf4c17cc4e24a324937fa521818

--- config/settings.py
+++ config/settings.py
@@ -60,6 +60,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USERNAME'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
