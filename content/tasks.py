f7eca5317a03a57dc7f16a9fc104a593b13ecc3b
Almaz <a.ilaletdinov@yandex.ru>
2020-08-09 12:26:17 -0400

    fix time in task
    Former-commit-id: 4719778be10c1eaa7e0e3aa69d78b5ffc6caa20d
    Former-commit-id: 43213951674ccf1e096e0ad0f0a80b7ca800b08e
    Former-commit-id: 134c2b21fd3fe8ec8cdf3920fab8a7092d5903f4

--- content/tasks.py
+++ content/tasks.py
@@ -5,8 +5,6 @@ from celery.schedules import crontab
 from content.service import do_morning_content_distribution
 
 
-# @periodic_task(run_every=(crontab(hour=7, minute=0)), name='mailing')
-# @periodic_task(run_every=(crontab(minute='*/1')), name='mailing')
-@periodic_task(run_every=(timedelta(seconds=5)), name='mailing')
+@periodic_task(run_every=(crontab(hour=7, minute=0)), name='mailing')
 def mailing():
     do_morning_content_distribution()
