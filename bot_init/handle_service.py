3fcd62ec7dc5970d3de7d5fd2c133e3b45ddd1a6
Almaz <a.ilaletdinov@yandex.ru>
2020-08-09 09:15:01 -0400

    add to favourites functional
    Former-commit-id: 12151e4c4c41b98e65ef5b02d5a98ec56b0b674c

--- bot_init/handle_service.py
+++ bot_init/handle_service.py
@@ -2,12 +2,23 @@
 import re
 
 from bot_init.text_message_service import translate_ayat_into_answer
+from bot_init.models import Subscriber
+from bot_init.service import get_tbot_instance
 from content.models import Ayat
 
 
-def handle_query_service(text: str):
+def handle_query_service(text: str, chat_id: int = None, call_id: int = None):
     if 'get_ayat' in text:
         ayat_pk = re.search(r'\d+', text).group(0)
         ayat = Ayat.objects.get(pk=ayat_pk)
         answer = translate_ayat_into_answer(ayat)
         return answer
+    elif 'add_in_favourites' in text:
+        ayat_pk = re.search(r'\d+', text).group(0)
+        ayat = Ayat.objects.get(pk=ayat_pk)
+        subscriber = Subscriber.objects.get(tg_chat_id=chat_id)
+        subscriber.favourite_ayats.add(ayat)
+        subscriber.save()
+        get_tbot_instance().answer_callback_query(call_id, show_alert=True, text='Аят добавлен в избранные')
+
+
