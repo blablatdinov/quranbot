ca33d0da4d98e16c996fe87df832a87f43e34e52
Almaz <a.ilaletdinov@yandex.ru>
2020-08-10 01:09:50 -0400

    come changes in admin and correct usibscribed action

--- bot_init/admin.py
+++ bot_init/admin.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 
 from bot_init.models import Message, Subscriber, Mailing, AdminMessage, SubscriberAction
 
+
 @admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'date', 'from_user_id', 'message_id', 'chat_id', 'get_message_text', )
-    search_fields = ('text', )
+    search_fields = ('text', 'from_user_id', 'chat_id')
 
     def get_message_text(self, obj):
         if isinstance(obj.text, str):
@@ -15,8 +16,21 @@ class MessageAdmin(admin.ModelAdmin):
     get_message_text.short_description = 'Текст'
 
 
-admin.site.register(Subscriber)
+@admin.register(Subscriber)
+class SubscriberAdmin(admin.ModelAdmin):
+    search_fields = (
+        'tg_chat_id',
+    )
+
 
 admin.site.register(Mailing)
 admin.site.register(AdminMessage)
-admin.site.register(SubscriberAction)
+
+
+@admin.register(SubscriberAction)
+class SubscriberActionAdmin(admin.ModelAdmin):
+    list_display = (
+        'subscriber',
+        'date_time',
+        'action'
+    )
