5e0b46dd56d533a868243c1e1f65ffb3a3a0cd74
almaz <a.ilaletdinov@yandex.ru>
2020-08-10 06:59:45 +0000

    add parser for prayer
    Former-commit-id: 7d013ac6231644ca453cf991c12f03637be46e17

--- config/settings.py
+++ config/settings.py
@@ -10,7 +10,7 @@ load_dotenv('.env')
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -61,12 +61,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USERNAME'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': '',
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': os.getenv('DB_NAME'),
+        # 'USER': os.getenv('DB_USERNAME'),
+        # 'PASSWORD': os.getenv('DB_PASSWORD'),
+        # 'HOST': 'localhost',
+        # 'PORT': '',
     }
 }
 
