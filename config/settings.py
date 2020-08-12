ab900edee5f9e01a024039524956104cdced2914
Almaz <a.ilaletdinov@yandex.ru>
2020-08-12 10:27:04 -0400

    change database on sqlite
    Former-commit-id: ca16c7616873f58f8ba5f20f1491754162cb3002

--- config/settings.py
+++ config/settings.py
@@ -59,14 +59,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USERNAME'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': os.getenv('DB_NAME'),
+        # 'USER': os.getenv('DB_USERNAME'),
+        # 'PASSWORD': os.getenv('DB_PASSWORD'),
+        # 'HOST': 'localhost',
+        # 'PORT': '',
     }
 }
 
