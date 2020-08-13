cfac1277e492628bd62d42763bee799339becea4
Almaz <a.ilaletdinov@yandex.ru>
2020-08-13 08:03:46 -0400

    add test for bot_init.service._create_action func
    Former-commit-id: 2499451b4ca8ce1e919677a1b24674aba086e53f
    Former-commit-id: e3d692dde71560c1c9aa5c9197426f4f645b7a36
    Former-commit-id: 8ba231bf564fbcd24a11dd5e7409b5143f91283c

--- bot_init/tests/test_bot_init_service.py
+++ bot_init/tests/test_bot_init_service.py
@@ -3,6 +3,8 @@ import os
 from dotenv import load_dotenv
 
 from bot_init.service import get_admins_list
+from bot_init.service import _create_action
+from bot_init.models import Subscriber, SubscriberAction
 from django.test import TestCase
 
 
@@ -15,4 +17,28 @@ class GetAdminListFuncTestCase(TestCase):
         admins_from_func = get_admins_list()
         admins_from_dotenv = [int(chat_id) for chat_id in os.getenv('ADMINS').split(',')]
         self.assertEqual(admins_from_dotenv, admins_from_func)
+
+
+class CreateActionTestCase(TestCase):
+
+    def test_subscribed(self):
+        subscriber = Subscriber.objects.create(tg_chat_id=2123)
+        action_name = 'subscribed'
+        res1 = SubscriberAction.objects.create(subscriber=subscriber, action=action_name)
+        res2 = SubscriberAction.objects.last()
+        self.assertEqual(res1, res2)
+
+    def test_unsubscribed(self):
+        subscriber = Subscriber.objects.create(tg_chat_id=2123)
+        action_name = 'unsubscribed'
+        res1 = SubscriberAction.objects.create(subscriber=subscriber, action=action_name)
+        res2 = SubscriberAction.objects.last()
+        self.assertEqual(res1, res2)
+
+    def test_reactivate(self):
+        subscriber = Subscriber.objects.create(tg_chat_id=2123)
+        action_name = 'reactivate'
+        res1 = SubscriberAction.objects.create(subscriber=subscriber, action=action_name)
+        res2 = SubscriberAction.objects.last()
+        self.assertEqual(res1, res2)
 
