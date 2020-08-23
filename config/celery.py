567c81cdeb08ecdb7b8bf2f07168ca6040560471
Almaz <a.ilaletdinov@yandex.ru>
2020-08-23 07:06:45 -0400

    add settings module for celery
    Former-commit-id: 34f61ef184d846889578f56632726717bf6db91d

--- config/celery.py
+++ config/celery.py
@@ -3,7 +3,11 @@ import os
 from celery import Celery
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+if os.getenv('DEBUG') == 'true':
+    using_settings = 'config.settings.dev'
+else:
+    using_settings = 'config.settings.prod'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', using_settings)
 
 app = Celery('config')
 
