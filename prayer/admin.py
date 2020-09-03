ed9a5412812ba7fe56a69c1dba5ecf444cf8c045
Almaz <a.ilaletdinov@yandex.ru>
2020-09-03 20:33:17 +0300

    Add docstring, delete not usable code and comments
    Former-commit-id: 2b309d2ca65f356c08eaafe6f5d7e624ad8ffdb5

--- prayer/admin.py
+++ prayer/admin.py
@@ -6,11 +6,10 @@ from prayer.models import City, Day, Prayer, PrayerAtUser, PrayerAtUserGroup
 admin.site.register(City)
 admin.site.register(Day)
 admin.site.register(Prayer)
-@admin.register(PrayerAtUser)
-class PrayerAtUserAdmin(admin.ModelAdmin):
-    list_display = ('__str__', 'is_read')
-
-
-
 admin.site.register(PrayerAtUserGroup)
 
+
+@admin.register(PrayerAtUser)
+class PrayerAtUserAdmin(admin.ModelAdmin):
+    """Настройки админки для модели намаза пользователя"""
+    list_display = ('__str__', 'is_read')
