412a6cf50a8b8e5b8436324b458f7bc3eba42a4d
Almaz <a.ilaletdinov@yandex.ru>
2020-12-19 12:33:56 +0300

    Форматирование по flake8
    Former-commit-id: 1f70f9b51be3260f2e8dc0bf44345025cdf7bfb5
    Former-commit-id: 6319ce929fbe69e0ef653abab7d930764136b054
    Former-commit-id: 5427990b926773c563e81ee3b389a130d0245a1a

--- config/celery.py
+++ config/celery.py
@@ -2,21 +2,21 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-# set the default Django settings module for the 'celery' program.
-if os.getenv('DEBUG') == 'true':
-    using_settings = 'config.settings.dev'
+# set the default Django settings module for the "celery" program.
+if os.getenv("DEBUG") == "true":
+    using_settings = "config.settings.dev"
 else:
-    using_settings = 'config.settings.prod'
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', using_settings)
+    using_settings = "config.settings.prod"
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", using_settings)
 
-app = Celery('config')
+app = Celery("config")
 
-# Using a string here means the worker doesn't have to serialize
+# Using a string here means the worker doesn"t have to serialize
 # the configuration object to child processes.
-# - namespace='CELERY' means all celery-related configuration keys
+# - namespace="CELERY" means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object("django.conf:settings", namespace="CELERY")
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
-app.conf.timezone = 'Europe/Moscow'
+app.conf.timezone = "Europe/Moscow"
--- config/settings/base.py
+++ config/settings/base.py
@@ -7,71 +7,71 @@ from loguru import logger
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-load_dotenv('.env')
+load_dotenv(".env")
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
 
-    'bot_init',
-    'content',
-    'prayer'
+    "bot_init",
+    "content",
+    "prayer"
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'config.urls'
+ROOT_URLCONF = "config.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'config.wsgi.application'
+WSGI_APPLICATION = "config.wsgi.application"
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
-LANGUAGE_CODE = 'ru-ru'
+LANGUAGE_CODE = "ru-ru"
 
-TIME_ZONE = 'Europe/Moscow'
+TIME_ZONE = "Europe/Moscow"
 
 USE_I18N = True
 
@@ -79,32 +79,32 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_URL = '/static/'
-STATIC_ROOT = 'static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = "static/"
 
-TG_BOT = namedtuple('Bot', ['token', 'webhook_host', 'name', 'id', 'admins'])
-TG_BOT.token = os.getenv('BOT_TOKEN')
-TG_BOT.webhook_host = os.getenv('HOST')
-r = requests.get(f'https://api.telegram.org/bot{TG_BOT.token}/getMe').json()
-if not r.get('ok'):
+TG_BOT = namedtuple("Bot", ["token", "webhook_host", "name", "id", "admins"])
+TG_BOT.token = os.getenv("BOT_TOKEN")
+TG_BOT.webhook_host = os.getenv("HOST")
+r = requests.get(f"https://api.telegram.org/bot{TG_BOT.token}/getMe").json()
+if not r.get("ok"):
     logger.info(r)
     exit()
 try:
-    if os.getenv('ADMINS') == '':
+    if os.getenv("ADMINS") == "":
         TG_BOT.admins = []
     else:
-        TG_BOT.admins = [int(chat_id) for chat_id in os.getenv('ADMINS').split(',')]
+        TG_BOT.admins = [int(chat_id) for chat_id in os.getenv("ADMINS").split(",")]
 except ValueError as e:
-    logger.error('Пожалуйста проверьте переменную ADMINS в файле .env')
+    logger.error("Пожалуйста проверьте переменную ADMINS в файле .env")
     raise e
 except AttributeError as e:
-    logger.error('Пожалуйста проверьте переменную ADMINS в файле .env')
+    logger.error("Пожалуйста проверьте переменную ADMINS в файле .env")
     raise e
-TG_BOT.name = r['result']['username']
-TG_BOT.id = r['result']['id']
+TG_BOT.name = r["result"]["username"]
+TG_BOT.id = r["result"]["id"]
 
 CELERY_BROKER_URL = os.getenv("REDIS_CONNECTION")
 
-CELERY_ACCEPT_CONTENT = ['json']
-CELERY_TASKS_SERIALIZER = 'json'
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASKS_SERIALIZER = "json"
 
