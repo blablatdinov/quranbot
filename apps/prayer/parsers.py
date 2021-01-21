adb43361d7be015c0559059454711ce3513b54aa
Almaz <a.ilaletdinov@yandex.ru>
2021-01-21 22:56:08 +0300

    #3 Исправлен парсер времен намазов
    1. Поправил импорты в файле management команды
    2. Исправил индекс для парсинга времен намазов из csv файла
    
    
    Former-commit-id: 1d01b0e6536dd1852b9f830d260f5a37474e5cf9
    Former-commit-id: 4a800177a25134b1ec06360898fe05c8330ac2de
    Former-commit-id: 789bbf8ba3cd5caa50d2c88678ec05bd0eb63c50

--- apps/prayer/parsers.py
+++ apps/prayer/parsers.py
@@ -1,6 +1,8 @@
 from datetime import datetime, time
 import csv
 import time as time_
+from loguru import logger
+from progressbar import progressbar as pbar
 
 import requests
 
@@ -15,14 +17,18 @@ def get_time_by_str(text: str) -> datetime:
 
 def prayer_time_parser():
     """Функция парсит данные с сайта http://dum.rt/."""
-    for city in City.objects.all():
+    logger.info(f"Count of city for parsing: {City.objects.count()}")
+    for i, city in enumerate(City.objects.all(), start=1):
+        logger.info(f"parsing prayer time for {i}. {city} city")
         r = requests.get(city.link_to_csv)
         decoded_content = r.content.decode("utf-8")
         csv_reader = csv.reader(decoded_content.splitlines(), delimiter=";")
-        for row in csv_reader:
+        for row in pbar(csv_reader):
             day, _ = Day.objects.get_or_create(date=datetime.strptime(row[0], "%d.%m.%Y"))
-            s = [1, 2, 4, 6, 7, 8]
+            s = [1, 3, 4, 6, 7, 8]
+            prayers = []
             for x in range(len(s)):
                 prayer_time = time_.strptime(row[s[x]], "%H:%M")
                 prayer_time = time(hour=prayer_time.tm_hour, minute=prayer_time.tm_min)
-                Prayer.objects.get_or_create(city=city, day=day, name=PRAYER_NAMES[x][0], time=prayer_time)
+                prayers.append(Prayer(city=city, day=day, name=PRAYER_NAMES[x][0], time=prayer_time))
+            Prayer.objects.bulk_create(prayers)
