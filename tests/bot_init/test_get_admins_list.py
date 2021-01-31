6f1a2ca424540a3571c4c73f34f361d5ad165975
Almaz <a.ilaletdinov@yandex.ru>
2021-01-31 19:32:31 +0300

    add pytest in req file and format tests
    Former-commit-id: 230cc63062d4916d1bf680bef41f318ebb4ccc14
    Former-commit-id: c7f9609ab9c62e4c352136b1423182ee995827c6
    Former-commit-id: 3f0366a268c8cf36066f8cca067907699448562f

--- tests/bot_init/test_get_admins_list.py
+++ tests/bot_init/test_get_admins_list.py
@@ -1,15 +1,10 @@
-import re
+import os
 
-from dotenv import load_dotenv
 import pytest
+from dotenv import load_dotenv
 
-from apps.bot_init.service import _subscriber_unsubscribed, _not_created_subscriber_service, _created_subscriber_service
-from apps.bot_init.service import *
-from apps.bot_init.service import _create_action
-from apps.bot_init.models import *
-from apps.bot_init.schemas import Answer
-from apps.content.models import Sura
-from django.conf import settings
+from apps.bot_init.models import Admin, Subscriber
+from apps.bot_init.service import get_admins_list
 
 pytestmark = [pytest.mark.django_db]
 
@@ -19,13 +14,15 @@ def admin_in_db():
     s = Subscriber.objects.create(tg_chat_id=123)
     return Admin.objects.create(subscriber=s)
 
+
 @pytest.fixture
 def admins_from_env():
-    load_dotenv('.env')
+    load_dotenv(".env")
     admins = [
-        int(chat_id) for chat_id in os.getenv('ADMINS').split(',')
-    ]    
+        int(chat_id) for chat_id in os.getenv("ADMINS").split(",")
+    ]
     return admins
+
 
 def test_get_admins_list(admins_from_env, admin_in_db):
     admins_from_func = get_admins_list()
