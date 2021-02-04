c40f50093c16ff9cb314b1633aad3f28f766119b
Almaz <a.ilaletdinov@yandex.ru>
2021-02-04 21:34:53 +0300

    end work 4.02.2021
    Former-commit-id: 98f7adc3d3a890a78ad3222ce745c38590f91c73

--- config/urls.py
+++ config/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('content/', include('apps.content.urls')),
     path('bot_init/', include('apps.bot_init.urls')),
+    path('api/', include('apps.api.urls')),
 ]
