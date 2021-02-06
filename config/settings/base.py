3d63d3cc6c10bec79e0b107d0302eb3c65875531
Almaz <a.ilaletdinov@yandex.ru>
2021-02-06 14:27:31 +0300

    add serializers
    Former-commit-id: 84fcd2ee7a33044785ed19311f96ea7aeaac64cd
    Former-commit-id: c0a8446f051163b75e5e5a9a0773dfb9cdce8f8f
    Former-commit-id: fbc8cceee899106275fb427162bee46dd93f2119

--- config/settings/base.py
+++ config/settings/base.py
@@ -19,6 +19,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
+    "rest_framework",
+
     "apps.api",
     "apps.bot_init",
     "apps.content",
@@ -109,3 +111,7 @@ CELERY_BROKER_URL = os.getenv("REDIS_CONNECTION")
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASKS_SERIALIZER = "json"
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100
+}
