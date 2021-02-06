462d6a1c7bae6dcb7b560c1dce1dc473cf6c2b99
Almaz <a.ilaletdinov@yandex.ru>
2021-02-06 17:07:19 +0300

    (#12) add method for parsing ayat content from exist html

--- apps/content/management/commands/pars_html.py
+++ apps/content/management/commands/pars_html.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
 
-from content.parsers import AyatParser
+from apps.content.parsers import AyatParser
 
 
 class Command(BaseCommand):
@@ -8,4 +8,4 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         p = AyatParser()
-        p.parse_content_from_ayats_in_db()
+        p.parse_content_from_db()
