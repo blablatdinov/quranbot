094f7a0de064e01fa113d4e78d9094a5cf180931
Almaz <a.ilaletdinov@yandex.ru>
2021-02-07 10:30:14 +0300

    change logic file
    Former-commit-id: 141882f89a71ff779c386a55d7ee16ccca9db3f6

--- deploy/restart.sh
+++ deploy/restart.sh
@@ -1,4 +1,8 @@
 cd /home/www/code/quranbot
 git pull
+source venv/bin/activate
+export DJANGO_SETTINGS_MODULE=config.settings.prod
+pip install -r /home/www/code/quranbot/requirements.txt
 /home/www/code/quranbot/venv/bin/python /home/www/code/quranbot/manage.py migrate
-supervisorctl restart qbot qbot_worker
+/home/www/code/quranbot/venv/bin/pytest
+#supervisorctl restart qbot qbot_worker
