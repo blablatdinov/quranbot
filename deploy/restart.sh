6fc0ad3ee41ac1e61d7b10ecd192ada6911093eb
Almaz <a.ilaletdinov@yandex.ru>
2021-02-08 10:25:52 +0300

    Update restart file
    Former-commit-id: 703ab7b6ad74726ff50d5feb7178b905450edf19
    Former-commit-id: 6447df9f3944cbe3c7cbde08246f78e882896501
    Former-commit-id: 9af549689c6a9cecc54f74943c1daed349886e7f

--- deploy/restart.sh
+++ deploy/restart.sh
@@ -2,7 +2,11 @@ cd /home/www/code/quranbot
 git pull
 source /home/www/code/quranbot/venv/bin/activate
 export DJANGO_SETTINGS_MODULE=config.settings.prod
+
+/home/www/code/quranbot/venv/bin/pip install -U pip
 /home/www/code/quranbot/venv/bin/pip install -r /home/www/code/quranbot/requirements.txt
 /home/www/code/quranbot/venv/bin/python /home/www/code/quranbot/manage.py migrate
-/home/www/code/quranbot/venv/bin/pytest
-supervisorctl restart qbot qbot_worker
+# /home/www/code/quranbot/venv/bin/pytest
+
+# supervisorctl restart qbot
+supervisorctl restart qbot_worker
