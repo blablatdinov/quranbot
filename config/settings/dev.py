d017e692ff0d39070d4cd01fc7ca84de630d9380
Almaz <a.ilaletdinov@yandex.ru>
2021-02-08 16:28:40 +0300

    work on logic in progress
    Former-commit-id: 7e899f76872410eda8f6a99e26afa9f9043eee3a
    Former-commit-id: 8263e7df28a45d265de7f898ef706c7c030f3320
    Former-commit-id: 0c45c577b46701336995e5191df264fd02e52593

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -9,13 +9,13 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USERNAME'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': '5123',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USERNAME'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'HOST': 'localhost',
+#         'PORT': '5123',
+#     }
+# }
