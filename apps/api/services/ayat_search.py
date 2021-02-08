ec0dbfcdd74690d3d331509f921ff889455f8359
Almaz <a.ilaletdinov@yandex.ru>
2021-02-08 10:40:54 +0300

    morning changes
    Former-commit-id: 0c8d7ff13350d2f21088e780ce5c1f58634cab39

--- apps/api/services/ayat_search.py
+++ apps/api/services/ayat_search.py
@@ -2,12 +2,55 @@ from loguru import logger
 
 from apps.content.models import Ayat
 
+from apps.bot_init.services.text_message_service import get_ayat_by_sura_ayat
+from apps.bot_init.exceptions import AyatDoesNotExists
 
-def get_ayat_by_sura_ayat_numbers(sura_number, ayat_number):
-    # TODO добавить тест на поиск 2:1
+
+def get_ayat_by_sura_ayat_numbers(sura_number: int, ayat_number: int):
     if sura_number and ayat_number:
         logger.info(f"Search {sura_number}:{ayat_number}")
         queryset = Ayat.objects.filter(sura__number=sura_number, ayat=ayat_number)
         return queryset
     queryset = Ayat.objects.all()
     return queryset
+
+
+class AyatSearcher:
+
+    def __init__(self, sura_number: int, ayat_number: int):
+        self.sura_number = sura_number
+        self.ayat_number = ayat_number
+
+    def _hyphen_cases(self, ayat):
+        low_limit, up_limit = [int(x) for x in str(ayat).split(':')[1].split('-')]
+        if ayat_num in range(low_limit, up_limit + 1):
+            return ayat
+
+    def _check_ayat(self):
+        ...
+
+    def get_ayat_by_sura_ayat(self) -> Ayat:
+        """
+        Функция возвращает аят по номеру суры и аята
+        Например: пользователь присылает 2:3, по базе ищется данный аят и возвращает 2:1-5
+        """
+
+        if not 1 <= self.sura_number <= 114:
+            raise SuraDoesNotExists
+
+        ayats_in_sura = Ayat.objects.filter(sura__number=sura_number)  # TODO разнести функцию, не читаемый код
+        for ayat in ayats_in_sura:
+            if '-' in str(ayat):  # Для кейсов типа 2:1-5
+                self._hyphen_cases(ayat)
+            elif ',' in str(ayat):  # Для кейсов типа 3:5,6
+                name = [int(x) for x in str(ayat).split(':')[1].split(',')]
+                if ayat_number in name:
+                    return ayat
+            elif int(ayat.ayat) == ayat_num:  # Для кейсов, когда название можно перевести в чиcло
+                return ayat
+        raise AyatDoesNotExists
+
+    def __call__(self):
+        raise AyatDoesNotExists
+        if self.sura_number and self.ayat_number:
+            ...
