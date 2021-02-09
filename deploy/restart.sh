c0653264267ae3360c80bbe35126919f549f4b36
blablatdinov <45946541+blablatdinov@users.noreply.github.com>
2021-02-09 08:06:51 +0300

    Update restart.sh
    Former-commit-id: f9998e132bec9ce46152cede80ea87e53d276416

--- deploy/restart.sh
+++ deploy/restart.sh
@@ -1,6 +1,6 @@
 cd /home/www/code/quranbot
 
-/home/www/code/quranbot/venv/bin/python /home/www/code/quranbot/manage.py dump
+# /home/www/code/quranbot/venv/bin/python /home/www/code/quranbot/manage.py dump
 
 git pull
 source /home/www/code/quranbot/venv/bin/activate
