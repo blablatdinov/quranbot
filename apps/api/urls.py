2721dcb5de16fea08c4eda4e7f08e9b8869bc48d
Almaz <a.ilaletdinov@yandex.ru>
2021-02-16 08:10:19 +0300

    (#10) Functional for getting prayer time, move serializers test in separated module

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from apps.api.views import AyatViewSet, PodcastViewSet
-
+from apps.api.views import AyatViewSet, PodcastViewSet, PrayerAtUserViewSet
 
 router = DefaultRouter()
 router.register(r"getAyat", AyatViewSet)
 router.register(r"getPodcast", PodcastViewSet)
+router.register(r"getPrayerAtUser", PrayerAtUserViewSet)
 
 urlpatterns = [
     path("v1/", include(router.urls))
--- apps/api/views.py
+++ apps/api/views.py
@@ -3,8 +3,9 @@ from rest_framework.permissions import AllowAny
 from loguru import logger
 
 from apps.content.services.podcast_services import get_random_podcast
-from apps.api.serializers import AyatSerializer, PodcastSerializer
+from apps.api.serializers import AyatSerializer, PodcastSerializer, PrayerAtUserSerializer
 from apps.content.models import Ayat, Podcast
+from apps.prayer.models import PrayerAtUser
 from apps.content.services.ayat_search import get_ayat_by_sura_ayat_numbers
 
 
@@ -24,3 +25,15 @@ class PodcastViewSet(viewsets.ReadOnlyModelViewSet):
 
     def get_queryset(self):
         return get_random_podcast()
+
+
+class PrayerAtUserViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+
+    Вьюха должна возвращать время намаза по chat_id или по городу
+    """
+    queryset = PrayerAtUser.objects.all()
+    serializer_class = PrayerAtUserSerializer
+
+    # def get_queryset(self):
+    #     ...
