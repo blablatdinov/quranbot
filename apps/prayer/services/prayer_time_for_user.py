b90b5aec6d740bdcce980006ac6dc8d44e19d66f
Almaz <a.ilaletdinov@yandex.ru>
2021-02-16 08:10:19 +0300

    (#10) Functional for getting prayer time, move serializers test in separated module
    Former-commit-id: bd47b03af3d8eca89f10ebb29e88953a9560bb58
    Former-commit-id: e1a4bd4d42718ef1abddf4e0bfb6fbdd785e1aab
    Former-commit-id: abd2df561a6edfd35f5b4b6ae519723daea9208b

---
+++ apps/prayer/services/prayer_time_for_user.py
@@ -0,0 +1,30 @@
+# FIXME maybe rename module
+from datetime import datetime, timedelta
+
+from django.db.models import QuerySet
+
+from apps.bot_init.service import get_subscriber_by_chat_id
+from apps.prayer.models import Prayer, City
+
+
+class PrayerAtUserGenerator:
+
+    def __init__(self, chat_id) -> None:
+        self.chat_id = chat_id
+        self._subscriber = get_subscriber_by_chat_id(chat_id)
+
+    @staticmethod
+    def get_prayer_time(city: City, date: datetime = datetime.today() + timedelta(days=1)) -> QuerySet:  # TODO а если нужно получать время намаза для определенного дня в API
+        """Возвращает время намазов для следующего дня."""
+        prayers = Prayer.objects.filter(city=city, day__date=date).order_by("pk")
+        return prayers
+
+    def __call__(self):
+        if self._subscriber.city is None:
+            return self._get_city_not_found_answer()
+        today = datetime.now()
+        prayers = self.get_prayer_time(self._subscriber.city, today)
+        return prayers
+
+    def _get_city_not_found_answer(self):
+        ...
