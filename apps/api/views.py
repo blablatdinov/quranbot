305023fe4afd6c2e5267d4d41fca34b60d36acf7
Almaz <a.ilaletdinov@yandex.ru>
2021-02-23 18:49:41 +0300

    (#10) create methods for set prayer status

--- apps/api/views.py
+++ apps/api/views.py
@@ -7,9 +7,9 @@ from django.utils.decorators import method_decorator
 from apps.content.services.podcast_services import get_random_podcast
 from apps.content.models import Ayat, Podcast
 from apps.content.services.ayat_search import get_ayat_by_sura_ayat_numbers
-from apps.api.serializers import AyatSerializer, PodcastSerializer, PrayerAtSubscriberSerializer, PrayerTimesSerializer
+from apps.api.serializers import AyatSerializer, PodcastSerializer, PrayerAtSubscriberSerializer, PrayerTimesSerializer, SetPrayerStatusSerializer, PrayerTimeAtUserInstanceSerializer
 from apps.api.exceptions.chat_id_or_city_not_gived import ChatIdOrCityNotGived, SubscriberNotDefinedCityAPIException
-from apps.prayer.models import PrayerAtUserGroup
+from apps.prayer.models import PrayerAtUserGroup, PrayerAtUser
 from apps.prayer.services.prayer_time_for_user import PrayerAtUserGenerator
 from apps.prayer.services.get_prayer_times_for_city import PrayerTimeGetter
 from apps.prayer.exceptions.subscriber_not_set_city import SubscriberNotSetCity
@@ -35,7 +35,7 @@ class PodcastViewSet(viewsets.ReadOnlyModelViewSet):
 
 
 @method_decorator(**get_prayer_time_doc)
-class PrayerTimeView(APIView):
+class PrayerTimeView(APIView):  # FIXME слишком толстые методы
     """
     Вьюха должна возвращать время намаза по chat_id или по городу
     """
@@ -49,7 +49,7 @@ class PrayerTimeView(APIView):
 
         if chat_id:
             try:
-                prayers_data = PrayerAtUserGenerator(int(chat_id))()
+                prayers_data = PrayerAtUserGenerator(chat_id=int(chat_id))()
                 serialized_data = PrayerAtSubscriberSerializer(
                     prayers_data
                 ).data
@@ -62,5 +62,16 @@ class PrayerTimeView(APIView):
                 prayers_data
             ).data
             from pprint import pprint
-            pprint(serialized_data)
             return Response(serialized_data)
+
+    def put(self, request):
+        serialized_data = SetPrayerStatusSerializer(data=request.data)
+        if serialized_data.is_valid():
+            serialized_data = serialized_data.data
+            prayer_at_user = PrayerAtUser.objects.get(id=serialized_data.get("id"))
+            prayer_at_user.is_read = serialized_data.get("is_read")
+            prayer_at_user.save()
+            serialized_data = PrayerTimeAtUserInstanceSerializer(prayer_at_user).data
+            return Response(serialized_data, status=201)
+        else:
+            logger.debug(f"{serialized_data.errors=}")
