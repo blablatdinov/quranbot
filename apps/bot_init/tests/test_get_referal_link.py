14091c9f342e2fa9a6272d34beaf09aa343ffff6
Almaz <a.ilaletdinov@yandex.ru>
2021-03-27 00:19:26 +0300

    functional ready

--- apps/bot_init/tests/test_get_referal_link.py
+++ apps/bot_init/tests/test_get_referal_link.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 import pytest
 from mixer.backend.django import mixer
+from django.conf import settings
 
 from apps.bot_init.service import get_referal_link
 
@@ -9,4 +11,4 @@ pytestmark = [pytest.mark.django_db]
 def test_get_referal_link(subscriber):
     answer = get_referal_link(subscriber.tg_chat_id)
 
-    assert answer.text == f"https://t.me/Quran_365_bot?start={subscriber.pk}"
+    assert answer.text == f"https://t.me/{settings.TG_BOT.name}?start={subscriber.pk}"
