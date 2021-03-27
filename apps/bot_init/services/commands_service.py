6a7e30e7303524f67b46928600385cbe4f627c44
Almaz <a.ilaletdinov@yandex.ru>
2021-03-27 17:12:11 +0300

    refactor commands service
    Former-commit-id: a7f0b4f164d074f5417d907a00465cb8e566693f
    Former-commit-id: f0e94bcf81e0f244bbb950d039f0b103bbb46b4e
    Former-commit-id: fac117e62eaceb3fef09826e4ded5e93e525828f

---
+++ apps/bot_init/services/commands_service.py
@@ -0,0 +1,80 @@
+from apps.bot_init.utils import save_message
+from apps.bot_init.models import Subscriber
+from loguru import logger
+
+from apps.bot_init.service import (
+    registration_subscriber, send_answer, send_message_to_admin,
+    _not_created_subscriber_service, _created_subscriber_service
+)
+from apps.bot_init.schemas import Answer
+
+
+class StartCommandService:
+    
+    def __init__(self, chat_id: int, message_text: str, additional_info: str = None):
+        self.chat_id = chat_id
+        self.message_text = message_text
+        self.additional_info = additional_info
+        self.referer = None
+
+    def __call__(self) -> Answer:
+        if self.additional_info:
+            self.referer = self.get_referer(self.additional_info)
+            logger.debug(f"Referal of new subscriber={self.additional_info}")
+        self.registration_subscriber()
+
+    def get_referer(self, referal_id):
+        try:
+            referer = Subscriber.objects.get(pk=int(referal_id))
+        except (Subscriber.DoesNotExist, ValueError) as e:
+            logger.error(f"Referer id={referal_id} does not exist. Error={str(e)}")
+            referer = None
+        return referer
+
+    def send_message_to_referer(self):
+        logger.debug(f"Send message to referer {self.referer.tg_chat_id=}")
+        message = Answer(text="По вашей реферальной ссылке произошла регистрация")
+        message = send_answer(message, self.referer.tg_chat_id)
+
+    def get_or_create_subscriber(self):
+        if (subscriber_query_set := Subscriber.objects.filter(tg_chat_id=self.chat_id)).exists():
+            logger.debug(f"This chat id was registered")
+            subscriber = subscriber_query_set.first()
+            created = False
+        else:
+            if self.referer:
+                self.send_message_to_referer()
+            subscriber = Subscriber.objects.create(
+                tg_chat_id=self.chat_id,
+                referer=self.referer,
+            )
+            created = True
+        return subscriber, created
+
+    def registration_subscriber(self):
+        """Логика сохранения подписчика."""
+        logger.debug(f"Registration subscriber with {self.chat_id=} {self.referer=}")
+        subscriber, created = self.get_or_create_subscriber()
+        if not created:
+            answer = _not_created_subscriber_service(subscriber)
+        else:
+            answer = _created_subscriber_service(subscriber)
+        return answer
+
+
+class CommandService:
+
+    def __init__(self, chat_id: int, message_text: str):
+        self.chat_id = chat_id
+        self.message_text = message_text
+        self.additional_info = self.get_additional_info()
+
+    def get_additional_info(self):
+        if (info := len(self.message_text.split())) > 1:
+            info = info[1]
+        logger.info(f"Command additional info={info}")
+        return info
+
+    def __call__(self) -> Answer:
+        if "start" in self.message_text:
+            StartCommandService(chat_id=self.chat_id, message_text=self.message_text, additional_info=self.additional_info)
