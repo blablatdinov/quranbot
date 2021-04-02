d4729e6ad76105e66794a925c26767ec8ba231c6
Almaz <a.ilaletdinov@yandex.ru>
2021-04-02 18:52:30 +0300

    refactor database and logs uploader
    Former-commit-id: d65878051e9f6509a21a96883a8b1dbd6473fe16
    Former-commit-id: 6a4fc935bd5a41b0ba772f7315e58a0db6af344a
    Former-commit-id: 704f49b526d764bdc10fabdae7b9a0278a9299ab

--- apps/bot_init/management/commands/dump.py
+++ apps/bot_init/management/commands/dump.py
@@ -1,10 +1,10 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 
-from apps.bot_init.service import upload_database_dump
+from apps.bot_init.services.db_dump_logs_uploader import DumpUploader
 
 
 class Command(BaseCommand):
     help = ''
 
-    def handle(self, *args, **kwargs):
-        upload_database_dump()
+    def handle(self):
+        DumpUploader()()
