80e252d72d62631a0c90c96c0b1623b119475263
Almaz <a.ilaletdinov@yandex.ru>
2021-04-02 12:07:58 +0300

    referer count info
    Former-commit-id: 5240c84ecc916b7610aeb92d0873509593ed3a0b

--- apps/bot_init/tests/test_concourse_button.py
+++ apps/bot_init/tests/test_concourse_button.py
@@ -1,7 +1,8 @@
 import pytest
-from mixer.backend.django import mixer
+from django.conf import settings
 
 from apps.bot_init.services.text_message_service import text_message_service
+from apps.bot_init.models import AdminMessage
 
 pytestmark = [pytest.mark.django_db]
 
@@ -9,4 +10,6 @@ pytestmark = [pytest.mark.django_db]
 def test_button_service(subscriber):
     answer = text_message_service(subscriber.tg_chat_id, "Конкурс")
 
-    assert "" in answer.text
+    assert AdminMessage.objects.get(key="concourse").text in answer.text
+    assert f"https://t.me/{settings.TG_BOT.name}?start={subscriber.pk}" in answer.text
+    assert "Кол-во пользователей зарегистрировавшихся по вашей ссылке: " in answer.text
