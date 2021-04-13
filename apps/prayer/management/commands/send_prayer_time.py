55da2c5dad630d6d11ab2a8b47a1c3461c579349
Almaz <a.ilaletdinov@yandex.ru>
2021-04-14 00:33:15 +0300

    delete import unused function
    Former-commit-id: 00044a76fe470b959835b5ef1dba270842069740
    Former-commit-id: 3ddf2302aa6b7778d29c3f43fb624cd75ff0480a
    Former-commit-id: 27a278e79e305b15913bcb1d7a48c546f2438892

--- apps/prayer/management/commands/send_prayer_time.py
+++ apps/prayer/management/commands/send_prayer_time.py
@@ -3,7 +3,6 @@ from datetime import datetime
 from django.core.management.base import BaseCommand
 
 from apps.prayer.service import send_prayer_time
-from apps.prayer.parsers import get_time_by_str
 
 
 class Command(BaseCommand):
