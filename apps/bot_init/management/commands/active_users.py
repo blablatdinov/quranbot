a0c2e21c9cf986a9b2159c663e3a7a31588f2d3e
Almaz <a.ilaletdinov@yandex.ru>
2021-05-14 09:47:25 +0300

    change count user to golang program
    Former-commit-id: 21f51cd6cbf9e446f2a6efb27694bd78218f5833

--- apps/bot_init/management/commands/active_users.py
+++ apps/bot_init/management/commands/active_users.py
@@ -3,10 +3,11 @@ import os
 from django.core.management.base import BaseCommand
 from loguru import logger
 
+from apps.bot_init.service import count_active_users
 
 class Command(BaseCommand):
     help = 'command return count of active users'
 
     def handle(self, *args, **options):
-        logger.info(os.system('./check_users'))
+        count_active_users()
 
