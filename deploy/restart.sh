e221ba8fcf84215b2b0771825369d2db590c099a
Almaz <a.ilaletdinov@yandex.ru>
2021-06-27 21:21:24 +0300

    change restart script [skip ci]
    Former-commit-id: e4f46faf7e7401665cdc00466a8348cae8d3e24b
    Former-commit-id: 8997a845c9ad5f5dc046a2569bf9c50a1bb0a0ab
    Former-commit-id: c75a56ec459884a59856c9f92cfe318a32055caa

--- deploy/restart.sh
+++ deploy/restart.sh
@@ -8,6 +8,7 @@ source /home/www/code/quranbot/venv/bin/activate
 export DJANGO_SETTINGS_MODULE=config.settings
 
 /home/www/code/quranbot/venv/bin/pip install -U pip
+/home/www/code/quranbot/venv/bin/pip uninstall -y $(/home/www/code/quranbot/venv/bin/pip freeze)
 /home/www/code/quranbot/venv/bin/pip install -r /home/www/code/quranbot/requirements.txt
 /home/www/code/quranbot/venv/bin/python /home/www/code/quranbot/manage.py migrate
 # /home/www/code/quranbot/venv/bin/pytest
