79a23ba5913abfc5d4f5b8a8fb482a714ac69e7b
Almaz <a.ilaletdinov@yandex.ru>
2021-07-07 21:21:12 +0300

    ayat api
    Former-commit-id: b533c23ef3e6ad6b9457b8e07a3e1561caec64ec

--- config/splitted_settings/rest_framework.py
+++ config/splitted_settings/rest_framework.py
@@ -1,6 +1,6 @@
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 100,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 50,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
---
+++ tests/api/content/test_get_ayats_which_not_used_in_morning_content.py
@@ -0,0 +1,21 @@
+import pytest
+from mixer.backend.django import mixer
+
+pytestmark = [pytest.mark.django_db]
+
+@pytest.fixture(autouse=True)
+def ayats():
+    mixer.cycle(7).blend(
+        'content.Ayat',
+        one_day_content=mixer.blend('content.MorningContent')
+    )
+    mixer.cycle(3).blend(
+        'content.Ayat',
+    )
+
+
+def test(client):
+    got = client.get('/api/v1/get-not-used-ayats/')
+
+    assert got.status_code == 200
+    assert len(got.json()['results']) == 3
