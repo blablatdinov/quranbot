9e04f4c654ec202fe7ad5784da203bb4f125e66a
Almaz <a.ilaletdinov@yandex.ru>
2021-07-11 16:11:19 +0300

    lint
    Former-commit-id: 08346e85e61ee6bab88e400d1f7ae528d79cab2d
    Former-commit-id: 4695cea44f85985cc69c969240688421b4110a1b
    Former-commit-id: d70d66475fe00340037ce385754a23f1eab78d90

--- config/splitted_settings/logger.py
+++ config/splitted_settings/logger.py
@@ -1,8 +1,8 @@
 from loguru import logger
 
 logger.add(
-    "logs/app.log",
-    format="{time:YYYY-MM-DD HH:mm:ss} {level} [{name}:{line}] {message}",
-    rotation="10:00"
+    'logs/app.log',
+    format='{time:YYYY-MM-DD HH:mm:ss} {level} [{name}:{line}] {message}',
+    rotation='10:00'
     # serialize=True,
 )
