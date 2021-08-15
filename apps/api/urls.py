04183ae40b1ed10d370df1279f23a24383d7102c
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2021-08-15 22:25:20 +0300

    ping message
    Former-commit-id: 0ae6d01261ba24279a65c952d38c57d9521202ac

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -18,10 +18,11 @@ urlpatterns = [
     path('v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('v1/morning-contents/', views.MorningContentView.as_view()),
     # path('v1/getPrayerTime/', views.PrayerTimeView.as_view()),
-    # path('v1/getAyat/', views.AyatAPIView.as_view()),
+    path('v1/get-ayat/', views.AyatAPIView.as_view()),
     # path('v1/getAyat/<int:pk>/', views.AyatDetailView.as_view()),
     path('v1/get-not-used-ayats/', views.NotUsedAyats.as_view()),
     path('v1/get-subscribers-count/', views.GetSubscribersCount.as_view()),
     path('v1/get-data-for-usage-graphic/', views.GetDataForUsageGraphic.as_view()),
+    path('v1/get-ping-to-message/', views.GetPingToMessage.as_view()),
     # path('v1/setPrayerStatus/', views.PrayerTimeView.as_view()),
 ] + api_docs_urls
