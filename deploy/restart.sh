b2839c9cb263f074b4f71fc0d211f24e411b2636
Almaz <a.ilaletdinov@yandex.ru>
2021-10-16 19:53:48 +0300

    update restart script [skip ci]

--- deploy/restart.sh
+++ deploy/restart.sh
@@ -4,8 +4,8 @@ git pull
 git reset --hard origin/master
 export DJANGO_SETTINGS_MODULE=config.settings
 
-poetry install
-poetry run python manage.py migrate
+/home/www/.poetry/bin/poetry install
+/home/www/.poetry/bin/poetry run python manage.py migrate
 
 supervisorctl restart qbot
 supervisorctl restart qbot_worker
