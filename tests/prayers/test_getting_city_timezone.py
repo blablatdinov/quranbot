3ea669400e411c09c7d92a67b8e4a20122d75a3e
Almaz Ilaletdinov <Almaz.Ilaletdinov@x5.ru>
2021-11-03 14:26:48 +0300

    fix test
    Former-commit-id: 7d8a0a365f51da161c63c072e9c29467f573a3f7

--- tests/prayers/test_getting_city_timezone.py
+++ tests/prayers/test_getting_city_timezone.py
@@ -1,4 +1,3 @@
-import requests_mock
 import pytest
 import pytz
 
@@ -8,7 +7,7 @@ from apps.prayer.service import get_city_timezone
 @pytest.mark.parametrize("city_name,timezone", [
     ("Владивосток", "Asia/Vladivostok"),
     ("Казань", "Europe/Moscow"),
-    ("Уфа", "Europe/Zurich"),
+    ("Уфа", "Asia/Yekaterinburg"),
 ])
 def test_get_city_timezone(city_name, timezone):  # FIXME замокай это
     got = get_city_timezone(city_name)
