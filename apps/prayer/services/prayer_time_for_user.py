57140f84978a21e49ee3fee3ba7550e599871eee
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2021-12-09 15:28:02 +0300

    lint prayers app

--- apps/prayer/services/prayer_time_for_user.py
+++ apps/prayer/services/prayer_time_for_user.py
@@ -1,9 +1,8 @@
 # FIXME maybe rename module
-from datetime import datetime, timedelta, tzinfo
+from datetime import datetime, timedelta
 
 import pytz
 from django.db.models import QuerySet
-from django.utils import timezone
 from loguru import logger
 
 from apps.bot_init.service import get_subscriber_by_chat_id
@@ -14,9 +13,10 @@ SUNRISE_INDEX = 1
 
 
 class PrayerAtUserGenerator:
+    """Генератор времени намаза для подписчика."""
 
     def __init__(
-        self, 
+        self,
         chat_id: int,
         day: str = "today",
     ) -> None:
@@ -25,16 +25,20 @@ class PrayerAtUserGenerator:
         self.day = day
 
     def localize_datetime(self, date_time):
+        """Локализовать время."""
         localized_time = date_time.astimezone(pytz.timezone("Europe/Moscow"))
         logger.debug(f"Localized datetime: {localized_time}")
         return datetime(localized_time.year, localized_time.month, localized_time.day)
 
     def get_prayer_time(
             self,
-            city: City, 
+            city: City,
             date: datetime,
-        ) -> QuerySet:  # TODO а если нужно получать время намаза для определенного дня в API
-        """Возвращает время намазов для следующего дня."""
+    ) -> QuerySet:
+        """Возвращает время намазов для следующего дня.
+
+        TODO а если нужно получать время намаза для определенного дня в API
+        """
         logger.debug(f"Getting prayers for {city.name}, date: {date}")
         prayers = Prayer.objects.filter(city=city, day__date=date).order_by("pk")
         prayer_group = PrayerAtUserGroup.objects.create()
@@ -42,20 +46,22 @@ class PrayerAtUserGenerator:
             PrayerAtUser.objects.create(
                 prayer=prayer,
                 prayer_group=prayer_group,
-                subscriber=self._subscriber
+                subscriber=self._subscriber,
             ) for prayer in prayers
         ]
 
     def set_attrs(self):
+        """Устанавливает атрибуты для класса."""
         self.city = self._subscriber.city.name
         self.subscriber_chat_id = self._subscriber.tg_chat_id
         self.sunrise_time = self.prayers[SUNRISE_INDEX].prayer.time
         self.prayers = [
-            prayer_time_at_user for prayer_time_at_user in self.prayers 
+            prayer_time_at_user for prayer_time_at_user in self.prayers
             if prayer_time_at_user.prayer.name != "sunrise"
         ]
 
     def get_date_by_day(self, day: str):
+        """Получить дату по строковому представлению."""
         date = {
             "today": self.time_zone.localize(datetime.now()),
             "tomorrow": self.time_zone.localize(datetime.now()) + timedelta(days=1),
@@ -63,6 +69,7 @@ class PrayerAtUserGenerator:
         return date
 
     def __call__(self):
+        """Entrypoint."""
         from apps.prayer.service import get_city_timezone
         logger.debug(f"Subscriber {self._subscriber} try get prayer_time. Subscriber city: {self._subscriber.city}")
 
@@ -85,6 +92,5 @@ class PrayerAtUserGenerator:
             text: "Вы не указали город, отправьте местоположение или воспользуйтесь поиском",
             button = InlineKeyboardButton("Поиск города", switch_inline_query_current_chat="")
         }
-
         """
         raise SubscriberNotSetCity
