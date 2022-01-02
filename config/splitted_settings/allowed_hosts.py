311072a7ff46bd2e88b9e32f7af747604f560a2e
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2022-01-02 18:43:50 +0300

    fix getting allowed hosts [skip ci]
    Former-commit-id: 73ff4f964f5f172a7308fee012215710cafbd0fe

--- config/splitted_settings/allowed_hosts.py
+++ config/splitted_settings/allowed_hosts.py
@@ -1,7 +1,7 @@
 from config.splitted_settings.boilerplate import DEBUG
 from config.splitted_settings.environ import env
 
-ALLOWED_HOSTS = [env('HOST').replace('http://', '').replace('https://', '')]
+ALLOWED_HOSTS = env('HOST', list)
 
 if DEBUG:
     ALLOWED_HOSTS += ['*']
