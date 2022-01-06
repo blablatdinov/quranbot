d82b560abeb20b74ba8129fefefd09e6529a17c5
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2022-01-06 17:27:59 +0300

    optimize usage data
    Former-commit-id: b749449b16cf8551f872b813e46791acc2e75439

--- tests/api/test_get_data_for_usage_graphic.py
+++ tests/api/test_get_data_for_usage_graphic.py
@@ -15,8 +15,9 @@ def messages(mixer):
     mixer.cycle(9).blend('bot_init.Message', from_user_id=333, date=pendulum.datetime(2021, 7, 30, tz='Europe/Moscow'))
 
 
-def test(client):
-    got = client.get('/api/v1/bot/get-data-for-usage-graphic/?dates_range=2021-07-26,2021-07-30')
+def test(client, django_assert_max_num_queries):
+    with django_assert_max_num_queries(1):
+        got = client.get('/api/v1/bot/get-data-for-usage-graphic/?dates_range=2021-07-26,2021-07-30')
 
     assert got.status_code == 200
     assert got.json() == [
