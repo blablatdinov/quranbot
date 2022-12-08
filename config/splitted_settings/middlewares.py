b064bbe9dd9016247fee344dd2f04781de65e74d
Almaz Ilaletdinov <a.ilaletdinov@yandex.ru>
2022-12-08 15:09:54 +0300

    uuids

--- config/splitted_settings/middlewares.py
+++ config/splitted_settings/middlewares.py
@@ -30,6 +30,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django_extensions',
 ]
 
 if DEBUG:
